95b5ce42450bb7c4e66a80176d8fcd5a40f70be5
Jonathan Gendron <65961361+jogendron@users.noreply.github.com>
2024-01-26 22:14:57 -0500

    Added support for keycloak idp

--- src/Spendings/Budget.Spendings.Api/Program.cs
+++ src/Spendings/Budget.Spendings.Api/Program.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Text.Json.Serialization;
+using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
 
 using Budget.Spendings.Api.Configuration;
@@ -26,8 +27,8 @@ builder.Configuration.AddUserSecrets(Assembly.GetCallingAssembly());
 DatabaseConfiguration databaseConfig = new DatabaseConfiguration();
 X509Certificate2? certificate = null;
 
-string? keyvaultUri = builder.Configuration.GetValue<string>("Keyvault:Uri");
-if (keyvaultUri != null)
+string? keyvaultUri = builder.Configuration.GetValue<string>("AzureKeyvault:Uri");
+if (! string.IsNullOrEmpty(keyvaultUri))
 {
     builder.Configuration.AddAzureKeyVault(
         new Uri(keyvaultUri),
@@ -52,9 +53,59 @@ if (! string.IsNullOrEmpty(certificateValue))
 */
 
 // API
-builder.Services.AddAuthentication(
-    JwtBearerDefaults.AuthenticationScheme
-).AddMicrosoftIdentityWebApi(builder.Configuration);
+var idp = builder.Configuration.GetValue<string>("IdentityProvider");
+switch (idp)
+{
+    case "EntraID":
+        builder.Services.AddAuthentication(
+            JwtBearerDefaults.AuthenticationScheme
+        ).AddMicrosoftIdentityWebApi(builder.Configuration, "EntraID");
+
+        break;
+
+    case "Keycloak":
+        builder.Services.AddAuthentication(options => {
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options => {
+            options.Authority = builder.Configuration.GetValue<string>("Keycloak:Authority");
+            options.Audience = builder.Configuration.GetValue<string>("Keycloak:Audience");
+            options.RequireHttpsMetadata = true;
+
+            if (builder.Configuration.GetValue<bool>("Keycloak:SetUsernameFromClaim"))
+            {
+                options.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = context => Task.Run(() =>
+                    {
+                        var claims = context?.Principal?.Claims.ToList();
+
+                        var nameClaim = context?.Principal?.FindFirst(
+                            builder.Configuration.GetValue<string>("Keycloak:UsernameClaim") ?? string.Empty
+                        );
+
+                        if (nameClaim != null)
+                        {
+                            var identity = context?.Principal?.Identity as ClaimsIdentity;
+                            identity?.AddClaim(new Claim(ClaimTypes.Name, nameClaim.Value));
+                        }
+                    })
+                };    
+            }
+
+            if (builder.Configuration.GetValue<bool>("Keycloak:IgnoreCertificateValidation"))
+            {
+                options.BackchannelHttpHandler = new HttpClientHandler()
+                {
+                    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                };
+            }          
+        });
+
+        break;
+
+    default:
+        throw new ArgumentException($"Unsupported IDP \"{idp}\"");
+};
 
 builder.Services.AddControllers().AddJsonOptions(options => 
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())
@@ -65,6 +116,7 @@ builder.Services.AddSwaggerGen();
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<IUserInspector, HttpContextUserInspector>();
 
 //Application
