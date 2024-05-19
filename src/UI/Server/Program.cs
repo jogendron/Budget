c3b51e53376438be90b7e1266eea54aef7b1eda5
Jonathan Gendron <65961361+jogendron@users.noreply.github.com>
2024-05-18 23:07:08 -0400

    Added server app for UI

---
+++ src/UI/Server/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Use(async (context, next) => {
+    var path = context.Request.Path.Value;
+    var physicalPath = Path.Join(app.Environment.WebRootPath, path);
+
+    if (string.IsNullOrEmpty(path) || path == "/" || (path.StartsWith("/en") && ! File.Exists(physicalPath))) 
+        context.Request.Path = "/en/index.html";
+    else if (path == "/fr" || (path.StartsWith("/fr") && ! File.Exists(physicalPath)))
+        context.Request.Path = "/fr/index.html";
+    else if (path.StartsWith("/assets"))
+        context.Request.Path = $"/en{path}";
+
+    await next(context);
+});
+
+app.UseStaticFiles();
+
+app.Run();
